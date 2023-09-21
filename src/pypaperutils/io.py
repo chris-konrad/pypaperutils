d8910456efc082942d4332d354d2bbce4080ff79
chrismo-schmidt <christophm.schmidt@posteo.nl>
2023-09-21 13:24:41 +0200

    added export to pgf

---
+++ src/pypaperutils/io.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 21 13:13:23 2023
+
+pypaperutils.io
+
+Export and import of figures and other elements
+
+@author: Christoph M. Schmidt
+"""
+
+import os
+
+def export_to_pgf(fig, filename, dirname=None, save=True): 
+    """Export a figure to pgf format so that latex may render text by itself.
+    
+
+    Parameters
+    ----------
+    fig : figure
+        Figure to be exportet.
+    filename : str
+        Name of the generated pfg file without ".pgf".
+    dirname : str, optional
+        Directory where the figure should be saved. If empty, the figure will 
+        be saved in the directory of the source file. 
+    save : boolean, optional
+        Disables saving. The default is True.
+
+    Returns
+    -------
+    None.
+
+    """
+    
+    if save:
+        if dirname is not None:
+            path = os.path.join(filename, dirname) + '.pgf'
+            if not os.path.exists(dirname):
+                os.makedirs()  
+        else:
+            path = filename + '.pgf'
+        fig.savefig(path)  
